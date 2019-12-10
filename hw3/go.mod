5dc97e13e48938c949236e5ec0c287fdb62bce98
Oxana Grishchenko <grishchenko@buroburo.ru>
2019-12-11 00:18:14 +0200

    hw3 base

---
+++ hw3/go.mod
@@ -0,0 +1,3 @@
+module github.com/LckDck/Go/hw3
+
+go 1.13
---
+++ hw3/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(frequency("asss ,sldfa sldfa sldfa alsdfa;, asdfa asdfa; alfs"))
+}
+
+func frequency(input string) string {
+	delimiters := []rune{' ', '.', ',', '!', '?', ':', ';', '(', ')', '"', '\'', '-'}
+	words := map[string]int{}
+	lastIndex := 0
+	wordReading := false
+
+	for i, r := range input {
+		if isDelimiter(r, delimiters) {
+			if wordReading {
+				word := input[lastIndex:i]
+				words[word]++
+			}
+			wordReading = false
+		} else {
+			if !wordReading {
+				lastIndex = i
+			}
+			wordReading = true
+		}
+	}
+	if wordReading {
+		word := input[lastIndex:len(input)]
+		words[word]++
+	}
+
+	max := 0
+	maxKey := ""
+	for key, val := range words {
+		if val > max {
+			max = val
+			maxKey = key
+		}
+	}
+
+	return maxKey
+}
+
+func isDelimiter(r rune, delimiters []rune) bool {
+	for _, delim := range delimiters {
+		if r == delim {
+			return true
+		}
+	}
+	return false
+}
