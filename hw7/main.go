887f0baba1b1ab7865b2f917d6aa7c28f0a3c553
Oxana Grishchenko <grishchenko@buroburo.ru>
2020-01-19 18:48:24 +0200

    args

--- hw7/main.go
+++ hw7/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/LckDck/go/hw7/copy"
 	flag "github.com/spf13/pflag"
 )
@@ -8,7 +10,12 @@ import (
 var offset *int64 = flag.Int64("offset", 0, "offset before copy")
 var limit *int64 = flag.Int64("limit", 0, "limit of copy size")
 
+// var fileFrom *string = flag.Int64("from", 0, "offset before copy")
+// var fileTo *string = flag.Int64("limit", 0, "limit of copy size")
+
 func main() {
 	flag.Parse()
-	copy.Copy("from.txt", "to.txt", *limit, *offset)
+	from := os.Args[1]
+	to := os.Args[2]
+	copy.Copy(from, to, *limit, *offset)
 }
